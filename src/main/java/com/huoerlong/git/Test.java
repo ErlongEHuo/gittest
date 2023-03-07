84b4df70fdd8a1b319a0c6706a85e0456c5a5f24
hel <18535061027@163.com>
2023-03-07 21:04:04 +0800

    first commit idea

---
+++ src/main/java/com/huoerlong/git/Test.java
@@ -0,0 +1,13 @@
+package com.huoerlong.git;
+
+/**
+ * @ClassName Test
+ * @Date 2023/3/7  20:46
+ * @Author huoerlong
+ * @Description TODO
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+    }
+}
