f2022849500e3705ddb11483b7d1634df9e9548f
hel <18535061027@163.com>
2023-03-07 21:05:31 +0800

    throe commit idea

--- src/main/java/com/huoerlong/git/Test.java
+++ src/main/java/com/huoerlong/git/Test.java
@@ -9,6 +9,6 @@ package com.huoerlong.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("测试git");
-        System.out.println("测试git2");
+        System.out.println("测试git3");
     }
 }
